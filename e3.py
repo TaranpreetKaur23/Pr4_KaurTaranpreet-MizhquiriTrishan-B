ea7e39c8b4731c6ce027bc8378d769666bdd64b8
Trishan Mabel Mizhquiri Valencia <145555290+Trishan1B@users.noreply.github.com>
2023-11-30 09:24:51 +0100

    Update e3.py

--- e3.py
+++ e3.py
@@ -7,9 +7,17 @@ Programa que mostra per pantalla la suma de tots els nombres senars i de tots el
 nombres parells inferiors a un número límit, que l’usuari introdueix per teclat.
 """
 try:
-    suma=int(input("Introdueix límit: "))
-    for i in range(suma,2):
-        sumaparells=i
-        print(sumaparells," ", end="")
+    suma_parells = 0
+    suma_senars = 0
+
+    limit = int(input("Introdueix un número límit: "))
+
+    for i in range(limit):
+        if i % 2 == 0:
+            suma_parells += i
+        else:
+            suma_senars += i
+
+    print(f"Si el límit és {limit}, sumaParells {suma_parells} i sumaSenars {suma_senars}")
 except ValueError:
     print("NO FUNCIONA BOBO QUE ERES")
