f1afc19257233320cba7f46aef3074ea55bca187
Trishan Mizhquiri <trishan.mizhquiri.7e6@itb.cat>
2024-05-02 11:59:10 +0200

    2/05/2024

---
+++ Pt1,2,3_KaurTaranpreet-MizhquiriTrishan-ASIXc1B/data_source.py
@@ -0,0 +1,10 @@
+def getDataFromKeyboard():
+    print("Introdueix les dades (i prem Enter per a finalitzar):")
+    dades = input()
+    return dades
+
+
+import requests
+
+def get_data__from_server():
+    return None
---
+++ Pt1,2,3_KaurTaranpreet-MizhquiriTrishan-ASIXc1B/main.py
@@ -0,0 +1,16 @@
+import crazy_words as cw
+import data_source as ds
+
+
+def crazyWord():
+    cw.crazy_words()
+
+
+crazyWord()
+
+
+def dataSource():
+    ds.getDataFromKeyboard()
+
+
+dataSource()
